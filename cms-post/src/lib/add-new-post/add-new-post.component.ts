04adb1937f4950b168a4cf17d0d1f04e367087c2
Durgeshl <Durgeshl@synergerp.com>
2023-03-01 10:46:21 +0530

    #changes: add new file

---
+++ cms-post/src/lib/add-new-post/add-new-post.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng-cms-add-new-post',
+  templateUrl: './add-new-post.component.html',
+  styleUrls: ['./add-new-post.component.scss'],
+})
+export class AddNewPostComponent {}
--- cms-post/src/lib/cms-post.module.ts
+++ cms-post/src/lib/cms-post.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CreateFormComponent } from './create-form/create-form.component';
+import { MatInputModule } from '@angular/material/input';
+import { AddNewPostComponent } from './add-new-post/add-new-post.component';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [CreateFormComponent],
+  imports: [CommonModule, MatInputModule],
+  declarations: [CreateFormComponent, AddNewPostComponent],
   exports: [CreateFormComponent],
 })
 export class CmsPostModule {}
